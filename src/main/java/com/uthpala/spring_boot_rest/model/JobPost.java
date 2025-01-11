a155177d2ea7251575d47552526fe79cd77b6a3a
Uthpala Samarakoon <uthpalakasun7@gmail.com>
2025-01-11 12:01:18 +0530

    data JPA and postgress database connection

--- src/main/java/com/uthpala/spring_boot_rest/model/JobPost.java
+++ src/main/java/com/uthpala/spring_boot_rest/model/JobPost.java
@@ -1,6 +1,9 @@
 package com.uthpala.spring_boot_rest.model;
 
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,15 +14,13 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
+@Entity
 public class JobPost {
 
-
+	@Id
 	private int postId;
 	private String postProfile; 
 	private String postDesc;
 	private Integer reqExperience;
 	private List<String> postTechStack;
-	
-
 }
